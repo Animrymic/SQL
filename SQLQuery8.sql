ba15cf7dabb1be420b5a255223aabf122b97e120
Animrymic <trojanovikdragan@gmail.com>
2026-05-04 20:39:24 +0200

    Delete SQLQuery8.sql

--- SQLQuery8.sql
+++
@@ -1,9 +0,0 @@
---WORKSHOP 4 
-
-alter table Products
-add constraint DF_Product_Price DEFAULT 0 FOR Price
-
-select * from Products
-
-alter table Products with check
-add constraint UC_Product_Name UNIQUE (Name)
