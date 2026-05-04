c82e503767911da942fe1484ae2b9173eb4e6856
Animrymic <trojanovikdragan@gmail.com>
2026-05-04 20:38:18 +0200

    Delete SQLQuery13 - Views.sql

--- SQLQuery13 - Views.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE VIEW [Total amount of orders per business entity] as
-select be.Name, sum(o.TotalPrice) as TotalPricePerEntity
-from Orders o
-join BusinessEntities be
-on o.BusinessEntityId = be.Id
-group by be.Name 
-
-select * from [Total amount of orders per business entity]
